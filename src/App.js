25324c0fd7575a732dce499b422b7c3c4482d650
realroy <roiboon.c@ku.th>
2017-11-18 07:20:50 +0700

    used mocked api and showed data in app component.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,44 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
+import logo from './logo.png'
+import './App.css'
 
 class App extends Component {
+
+  state = {
+    announcers: []
+  }
+
+  componentWillMount() {
+    fetch('announcement.json')
+      .then(res => res.json())
+      .then(json => this.setState({ announcers: json }))
+      .catch(console.error)
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <img src={logo} className="App-logo" alt="logo"/>
+          <h1 className="App-title">Semi-Final Round</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div>
+          <button>Web Content</button>
+          <button>Web Design</button>
+          <button>Web Marketing</button>
+          <button>Web Programing</button>
+        </div>
+        <div>
+          {this.state.announcers.map(({firstName, lastName, major}, i) => (
+            <div key={i}>
+              <div>Name: {firstName} {lastName}</div>
+              <div>Major: {major[0].toUpperCase()+major.slice(1)}</div>
+            </div>
+          ))}
+        </div>
       </div>
     );
   }
 }
 
-export default App;
+export default App
